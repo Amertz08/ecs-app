435d1e7520000f6ed71b3a0ad36ff4b371f98290
Adam Mertz <adammertz@gmail.com>
2021-07-13 18:56:16 -0500

    Start ECS cluster

---
+++ infra/ecs/cluster/main.tf
@@ -0,0 +1,48 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "terraform_remote_state" "vpc_state" {
+  backend = "local"
+  config = {
+    path = "${path.module}/../../vpc/terraform.tfstate"
+  }
+}
+
+resource "aws_launch_template" "main" {
+  name = "ecs-app-lt"
+  instance_type = "t2.micro"
+  image_id = "ami-091aa67fccd794d5f"
+
+  placement {
+    availability_zone = "us-east-1b"
+  }
+
+  tags = {
+    Name    = "ECS App"
+    managed = "Terraform"
+  }
+}
+
+resource "aws_autoscaling_group" "main" {
+  max_size = 3
+  min_size = 0
+  desired_capacity = 0
+  vpc_zone_identifier = [data.terraform_remote_state.vpc_state.outputs.private_subnet_id]
+
+  launch_template {
+    id = aws_launch_template.main.id
+  }
+}
+
+resource "aws_ecs_capacity_provider" "main" {
+  name = "app-cap-provider"
+  auto_scaling_group_provider {
+    auto_scaling_group_arn = aws_autoscaling_group.main.arn
+  }
+}
+
+resource "aws_ecs_cluster" "main" {
+  name = "ecs-app-cluster"
+  capacity_providers = [aws_ecs_capacity_provider.main.name]
+}
