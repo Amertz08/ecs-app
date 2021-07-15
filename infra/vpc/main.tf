f457f72a7b24db76fff7eab2eaeceb4336e2f1d2
Adam Mertz <adammertz@gmail.com>
2021-07-14 20:48:52 -0500

    typo fix

--- infra/vpc/main.tf
+++ infra/vpc/main.tf
@@ -82,7 +82,7 @@ resource "aws_route_table" "public" {
   }
 }
 
-resource "aws_route" "pubic" {
+resource "aws_route" "public" {
   route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.public.id
