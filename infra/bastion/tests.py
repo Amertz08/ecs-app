93ea98d9c34c368c64925d727a12120bef45c142
Adam Mertz <adammertz@gmail.com>
2021-07-15 22:30:19 -0500

    move pem key

--- infra/bastion/tests.py
+++ infra/bastion/tests.py
@@ -3,29 +3,28 @@ import pytest
 
 
 class SSHManager:
-    def __init__(self):
+    def __init__(self, pkey: paramiko.RSAKey):
+        self.pkey = pkey
         self.client = paramiko.SSHClient()
         self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
     def __enter__(self):
         return self
 
-    def __call__(self, hostname: str, username: str, pkey: paramiko.RSAKey):
-        self.client.connect(hostname=hostname, username=username, pkey=pkey, timeout=5)
+    def __call__(self, hostname: str, username: str):
+        self.client.connect(
+            hostname=hostname, username=username, pkey=self.pkey, timeout=5
+        )
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.client.close()
 
 
 @pytest.fixture()
-def pem_key(request):
+def ssh_manager(request) -> SSHManager:
     key_path = request.config.getoption("--pem-key-path")
-    return paramiko.RSAKey.from_private_key_file(key_path)
-
-
-@pytest.fixture()
-def ssh_manager() -> SSHManager:
-    man = SSHManager()
+    pkey = paramiko.RSAKey.from_private_key_file(key_path)
+    man = SSHManager(pkey)
     return man
 
 
@@ -36,8 +35,7 @@ def remote_command_exit_code(client: paramiko.SSHClient, cmd: str) -> int:
     return exit_code
 
 
-def test_can_ssh_into_bastion(bastion_ip, ssh_manager, pem_key):
-
-    with ssh_manager(hostname=bastion_ip, username="ec2-user", pkey=pem_key) as client:
+def test_can_ssh_into_bastion(bastion_ip, ssh_manager):
+    with ssh_manager(hostname=bastion_ip, username="ec2-user") as client:
         exit_code = remote_command_exit_code(client, "ps")
     assert exit_code == 0
