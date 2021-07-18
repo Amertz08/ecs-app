85bae8917084ee70dcda54ecd57f9bbde80e43c6
Adam Mertz <adammertz@gmail.com>
2021-07-17 20:49:12 -0500

    start using terragrunt

--- infra/spin-down.sh
+++ infra/spin-down.sh
@@ -3,13 +3,13 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 cd $SCRIPT_DIR/ecs/cluster
-terraform apply -auto-approve
+terragrunt apply -auto-approve
 cd $SCRIPT_DIR
 
 cd $SCRIPT_DIR/bastion
-terraform apply -auto-approve
+terragrunt apply -auto-approve
 cd $SCRIPT_DIR
 
 cd $SCRIPT_DIR/vpc
-terraform apply -auto-approve
+terragrunt apply -auto-approve
 cd $SCRIPT_DIR
