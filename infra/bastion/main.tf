a93b84289aea71a5df62ba9195956a1026ab0c38
Adam Mertz <adammertz@gmail.com>
2021-11-06 12:18:22 -0500

    start using new security groups in bastion

--- infra/bastion/main.tf
+++ infra/bastion/main.tf
@@ -8,6 +8,16 @@ data "terraform_remote_state" "vpc" {
   }
 }
 
+data "terraform_remote_state" "security_groups" {
+  backend = "s3"
+  config = {
+    region  = "us-east-1"
+    profile = "default"
+    key     = "ecs-app/security_groups/terraform.tfstate"
+    bucket  = "tf-state-personal-projects"
+  }
+}
+
 data aws_ssm_parameter "amz_linux_ami" {
   name = "/aws/service/ami-amazon-linux-latest/amzn-ami-hvm-x86_64-gp2"
 }
@@ -22,13 +32,16 @@ module "bastion_asg" {
   name     = "bastion"
   image_id = data.aws_ssm_parameter.amz_linux_ami.value
 
-  is_public           = true
-  instance_type       = "t2.micro"
-  key_name            = "adam-mbp"
-  desired_capacity    = var.bastion_instance_count
-  min_size            = var.bastion_instance_count
-  max_size            = 4
-  ssh_cidr_blocks     = ["0.0.0.0/0"]
+  is_public        = true
+  instance_type    = "t2.micro"
+  key_name         = "adam-mbp"
+  desired_capacity = var.bastion_instance_count
+  min_size         = var.bastion_instance_count
+  max_size         = 4
+  security_groups = [
+    data.terraform_remote_state.security_groups.outputs.public_instance_sg_id,
+    data.terraform_remote_state.security_groups.outputs.ssh_self_sg_id
+  ]
   vpc_id              = data.terraform_remote_state.vpc.outputs.vpc_id
   vpc_zone_identifier = data.terraform_remote_state.vpc.outputs.public_subnet_ids
 }
