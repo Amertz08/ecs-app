dd18aba70f1e57875c9e39386e47b92becb6fb15
Adam Mertz <adammertz@gmail.com>
2021-11-06 12:32:13 -0500

    get ecs cluster going

--- infra/ecs/cluster/main.tf
+++ infra/ecs/cluster/main.tf
@@ -8,6 +8,16 @@ data "terraform_remote_state" "vpc" {
   }
 }
 
+data "terraform_remote_state" "security_groups" {
+  backend = "s3"
+  config = {
+    region  = "us-east-1"
+    profile = "default"
+    key     = "ecs-app/security_groups/terraform.tfstate"
+    bucket  = "tf-state-personal-projects"
+  }
+}
+
 variable "cluster_instance_count" {
   type    = number
   default = 0
@@ -15,16 +25,17 @@ variable "cluster_instance_count" {
 
 module "ecs-app-cluster" {
 
-  source                    = "../../modules/ecs_cluster"
-  name                      = "apps"
-  desired_capacity          = var.cluster_instance_count
-  min_size                  = var.cluster_instance_count
-  max_size                  = 4
-  instance_type             = "t2.micro"
-  key_name                  = "adam-mbp"
-  vpc_id                    = data.terraform_remote_state.vpc.outputs.vpc_id
-  vpc_zone_identifier       = data.terraform_remote_state.vpc.outputs.private_subnet_ids
-  public_subnet_cidr_blocks = data.terraform_remote_state.vpc.outputs.public_subnet_cidr_blocks
+  source              = "../../modules/ecs_cluster"
+  name                = "apps"
+  desired_capacity    = var.cluster_instance_count
+  min_size            = var.cluster_instance_count
+  max_size            = 4
+  instance_type       = "t2.micro"
+  key_name            = "adam-mbp"
+  vpc_zone_identifier = data.terraform_remote_state.vpc.outputs.private_subnet_ids
+  security_groups = [
+    data.terraform_remote_state.security_groups.outputs.ssh_self_sg_id
+  ]
 }
 
 output "cluster_arn" {
